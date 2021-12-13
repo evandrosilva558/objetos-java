2aeb9eba96ac6a4f49365ae532b67966fbbda5e2
Evandro <evandrosilva558@hotmail.com>
2021-12-13 19:49:28 -0300

    criando o objeto aluno e instanciando seus atributos com o metodo SET.

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -1,5 +1,101 @@
 package cursojava.classes;
 
 public class Aluno {
+	
+	//	stributos da classe.
+	
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+	
+	// methodos Getters e Setters recebe e obtem os valores do objeto Aluno.
+	// Set = seta os valores recebe valor.
+	// Get = obtem os valores setados ao objeto.
+	
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
 
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getRegistroGerla() {
+		return registroGeral;
+	}
+
+	public void setRegistroGerla(String registroGerla) {
+		this.registroGeral = registroGerla;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
--- src/cursojava/executavel/ClasseExecutavel.java
+++ src/cursojava/executavel/ClasseExecutavel.java
@@ -1,11 +1,25 @@
 package cursojava.executavel;
 
+import cursojava.classes.Aluno;
+
 public class ClasseExecutavel {
 
 	public static void main(String[] args) {
 		
+		// Setando os valores dos atributos do objeto aluno.
 		
-
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Alex");
+		aluno1.setIdade(30);
+		aluno1.setDataNascimento("11/10/1998");
+		aluno1.setRegistroGerla("482144428");
+		aluno1.setNumeroCpf("14141257890");
+		aluno1.setNomeMae("Maria Jose do Alto");
+		aluno1.setNomePai("Antoni Jose");
+		aluno1.setDataMatricula("5/2/2020");
+		aluno1.setNomeEscola("Uninove");
+		aluno1.setSerieMatriculado("TADS-26");
+		
 	}
 
 }
